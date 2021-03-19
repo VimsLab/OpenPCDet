140bfe4002d9bd6b69323cefe3c6bf1a67c82dac
Maxim Bazik <maxbazik@gauss.cis.udel.edu>
2021-03-19 13:57:42 -0400

    Initial changes to PV-RCNN.

--- pcdet/models/backbones_3d/spconv_backbone.py
+++ pcdet/models/backbones_3d/spconv_backbone.py
@@ -2,6 +2,7 @@ from functools import partial
 
 import spconv
 import torch.nn as nn
+import torch
 
 
 def post_act_block(in_channels, out_channels, kernel_size, indice_key=None, stride=1, padding=0,
@@ -243,6 +244,270 @@ class VoxelResBackBone8x(nn.Module):
 
         # for detection head
         # [200, 176, 5] -> [200, 176, 2]
+        out = self.conv_out(x_conv4)
+
+        batch_dict.update({
+            'encoded_spconv_tensor': out,
+            'encoded_spconv_tensor_stride': 8
+        })
+        batch_dict.update({
+            'multi_scale_3d_features': {
+                'x_conv1': x_conv1,
+                'x_conv2': x_conv2,
+                'x_conv3': x_conv3,
+                'x_conv4': x_conv4,
+            }
+        })
+
+        return batch_dict
+
+def sum_duplicates(t1, t2):
+    new_indices, inverse_mapping, count = torch.unqiue(torch.cat(t1.indices,t2.indices),dim=0, return_inverse=True, return_counts=True)
+    new_features = torch.zeros((new_indices.size()[0], t1.features.size()[1]))
+    t1.index()
+
+class VoxelBackBoneHiRes(nn.Module):
+    def __init__(self, model_cfg, input_channels, grid_size, **kwargs):
+        super().__init__()
+        self.alpha = 0.5 # hardcoded
+        self.model_cfg = model_cfg
+        self.sparse_shape = grid_size[::-1] + [1, 0, 0]
+
+        alpha = 0.5
+        level = 0
+        norm_fn = partial(nn.BatchNorm1d, eps=1e-3, momentum=0.01)
+        sf = int(1/alpha**level)
+        pool = spconv.SparseMaxPool3d(sf, sf)
+        self.l0_conv_input = spconv.SparseSequential(
+            pool,
+            spconv.SubMConv3d(input_channels, 16*sf, 3, padding=1, bias=False, indice_key='subm1'),
+            norm_fn(16*sf),
+            nn.ReLU(),
+        )
+        block = post_act_block
+        self.l0_conv1 = spconv.SparseSequential(
+            block(16*sf, 16*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm1'),
+        )
+        self.l0_conv2 = spconv.SparseSequential(
+            # [1600, 1408, 41] <- [800, 704, 21]
+            block(16*sf, 32*sf, 3, norm_fn=norm_fn, stride=2, padding=1, indice_key='spconv2', conv_type='spconv'),
+            block(32*sf, 32*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm2'),
+            block(32*sf, 32*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm2'),
+        )
+        self.l0_conv3 = spconv.SparseSequential(
+            # [800, 704, 21] <- [400, 352, 11]
+            block(32*sf, 64*sf, 3, norm_fn=norm_fn, stride=2, padding=1, indice_key='spconv3', conv_type='spconv'),
+            block(64*sf, 64*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm3'),
+            block(64*sf, 64*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm3'),
+        )
+        self.l0_conv4 = spconv.SparseSequential(
+            # [400, 352, 11] <- [200, 176, 5]
+            block(64*sf, 128*sf, 3, norm_fn=norm_fn, stride=2, padding=(0, 1, 1), indice_key='spconv4', conv_type='spconv'),
+            block(128*sf, 128*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm4'),
+            block(128*sf, 128*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm4'),
+        )
+
+        level = 1
+        norm_fn = partial(nn.BatchNorm1d, eps=1e-3, momentum=0.01)
+        sf = int(1/alpha**level)
+        pool = spconv.SparseMaxPool3d(sf, sf, padding=sf//2)
+        self.l1_conv_input = spconv.SparseSequential(
+            pool,
+            spconv.SubMConv3d(input_channels, 16*sf, 3, padding=1, bias=False, indice_key='subm1'),
+            norm_fn(16*sf),
+            nn.ReLU(),
+        )
+        block = post_act_block
+        self.l1_conv1 = spconv.SparseSequential(
+            block(16*sf, 16*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm1'),
+        )
+        self.l1_conv2 = spconv.SparseSequential(
+            # [1600, 1408, 41] <- [800, 704, 21]
+            block(16*sf, 32*sf, 3, norm_fn=norm_fn, stride=2, padding=1, indice_key='spconv2', conv_type='spconv'),
+            block(32*sf, 32*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm2'),
+            block(32*sf, 32*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm2'),
+        )
+        self.l1_conv3 = spconv.SparseSequential(
+            # [800, 704, 21] <- [400, 352, 11]
+            block(32*sf, 64*sf, 3, norm_fn=norm_fn, stride=2, padding=1, indice_key='spconv3', conv_type='spconv'),
+            block(64*sf, 64*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm3'),
+            block(64*sf, 64*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm3'),
+        )
+        self.l1_conv4 = spconv.SparseSequential(
+            # [400, 352, 11] <- [200, 176, 5]
+            block(64*sf, 128*sf, 3, norm_fn=norm_fn, stride=2, padding=(1, 1, 1), indice_key='spconv4', conv_type='spconv'),
+            block(128*sf, 128*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm4'),
+            block(128*sf, 128*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm4'),
+        )
+
+        level = 2
+        norm_fn = partial(nn.BatchNorm1d, eps=1e-3, momentum=0.01)
+        sf = int(1/alpha**level)
+        pool = spconv.SparseMaxPool3d(sf, sf,  padding=sf//2)
+        self.l2_conv_input = spconv.SparseSequential(
+            pool,
+            spconv.SubMConv3d(input_channels, 16*sf, 3, padding=1, bias=False, indice_key='subm1'),
+            norm_fn(16*sf),
+            nn.ReLU(),
+        )
+        block = post_act_block
+        self.l2_conv1 = spconv.SparseSequential(
+            block(16*sf, 16*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm1'),
+        )
+        self.l2_conv2 = spconv.SparseSequential(
+            # [1600, 1408, 41] <- [800, 704, 21]
+            block(16*sf, 32*sf, 3, norm_fn=norm_fn, stride=2, padding=1, indice_key='spconv2', conv_type='spconv'),
+            block(32*sf, 32*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm2'),
+            block(32*sf, 32*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm2'),
+        )
+        self.l2_conv3 = spconv.SparseSequential(
+            # [800, 704, 21] <- [400, 352, 11]
+            block(32*sf, 64*sf, 3, norm_fn=norm_fn, stride=2, padding=1, indice_key='spconv3', conv_type='spconv'),
+            block(64*sf, 64*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm3'),
+            block(64*sf, 64*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm3'),
+        )
+        self.l2_conv4 = spconv.SparseSequential(
+            # [400, 352, 11] <- [200, 176, 5]
+            block(64*sf, 128*sf, 3, norm_fn=norm_fn, stride=2, padding=(1, 1, 1), indice_key='spconv4', conv_type='spconv'),
+            block(128*sf, 128*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm4'),
+            block(128*sf, 128*sf, 3, norm_fn=norm_fn, padding=1, indice_key='subm4'),
+        )
+
+        last_pad = 0
+        last_pad = self.model_cfg.get('last_pad', last_pad)
+        norm_fn = partial(nn.BatchNorm1d, eps=1e-3, momentum=0.01)
+        self.conv_out = spconv.SparseSequential(
+            # [200, 150, 5] -> [200, 150, 2]
+            spconv.SparseConv3d(128, 128, (3, 1, 1), stride=(2, 1, 1), padding=last_pad,
+                                bias=False, indice_key='spconv_down2'),
+            norm_fn(128),
+            nn.ReLU(),
+        )
+
+        self.num_point_features = 128
+
+        I=3
+        J=4
+
+        self.obo_0_0 = spconv.SparseConv3d(16*(2**0)*(2**0),16*2**(J-1)*2**(I-1),1)
+        self.obo_0_1 = spconv.SparseConv3d(16*(2**1)*(2**0),16*2**(J-1)*2**(I-1),1)
+        self.obo_0_2 = spconv.SparseConv3d(16*(2**2)*(2**0),16*2**(J-1)*2**(I-1),1)
+        self.obo_0_3 = spconv.SparseConv3d(16*(2**3)*(2**0),16*2**(J-1)*2**(I-1),1)
+        self.obo_1_0 = spconv.SparseConv3d(16*(2**0)*(2**1),16*2**(J-1)*2**(I-1),1)
+        self.obo_1_1 = spconv.SparseConv3d(16*(2**1)*(2**1),16*2**(J-1)*2**(I-1),1)
+        self.obo_1_2 = spconv.SparseConv3d(16*(2**2)*(2**1),16*2**(J-1)*2**(I-1),1)
+        self.obo_1_3 = spconv.SparseConv3d(16*(2**3)*(2**1),16*2**(J-1)*2**(I-1),1)
+        self.obo_2_0 = spconv.SparseConv3d(16*(2**0)*(2**2),16*2**(J-1)*2**(I-1),1)
+        self.obo_2_1 = spconv.SparseConv3d(16*(2**1)*(2**2),16*2**(J-1)*2**(I-1),1)
+        self.obo_2_2 = spconv.SparseConv3d(16*(2**2)*(2**2),16*2**(J-1)*2**(I-1),1)
+        self.obo_2_3 = spconv.SparseConv3d(16*(2**3)*(2**2),16*2**(J-1)*2**(I-1),1)
+
+
+        self.pool_2 = spconv.SparseMaxPool3d(2,stride=1)
+
+        self.final_convs_0 = spconv.SparseConv3d(16*2**(J-1)*2**(I-1),16*2**0,1)
+        self.final_convs_1 = spconv.SparseConv3d(16*2**(J-1)*2**(I-1),16*2**1,1)
+        self.final_convs_2 = spconv.SparseConv3d(16*2**(J-1)*2**(I-1),16*2**2,1)
+        self.final_convs_3 = spconv.SparseConv3d(16*2**(J-1)*2**(I-1),16*2**3,1)
+
+    def forward(self, batch_dict):
+        print("FORWARD?")
+        """
+        Args:
+            batch_dict:
+                batch_size: int
+                vfe_features: (num_voxels, C)
+                voxel_coords: (num_voxels, 4), [batch_idx, z_idx, y_idx, x_idx]
+        Returns:
+            batch_dict:
+                encoded_spconv_tensor: sparse tensor
+        """
+        voxel_features, voxel_coords = batch_dict['voxel_features'], batch_dict['voxel_coords']
+        batch_size = batch_dict['batch_size']
+        input_sp_tensor0 = spconv.SparseConvTensor(
+            features=voxel_features,
+            indices=voxel_coords.int(),
+            spatial_shape=self.sparse_shape,
+            batch_size=batch_size
+        )
+
+        x0 = self.l0_conv_input(input_sp_tensor0)
+        x0_conv1 = self.l0_conv1(x0)
+        x0_conv2 = self.l0_conv2(x0_conv1)
+        x0_conv3 = self.l0_conv3(x0_conv2)
+        x0_conv4 = self.l0_conv4(x0_conv3)
+    
+
+        input_sp_tensor1 = spconv.SparseConvTensor(
+            features=voxel_features,
+            indices=voxel_coords.int(),
+            spatial_shape=self.sparse_shape,
+            batch_size=batch_size
+        )
+        x1 = self.l1_conv_input(input_sp_tensor1)
+        x1_conv1 = self.l1_conv1(x1)
+        x1_conv2 = self.l1_conv2(x1_conv1)
+        x1_conv3 = self.l1_conv3(x1_conv2)
+        x1_conv4 = self.l1_conv4(x1_conv3)
+
+        input_sp_tensor2 = spconv.SparseConvTensor(
+            features=voxel_features,
+            indices=voxel_coords.int(),
+            spatial_shape=self.sparse_shape,
+            batch_size=batch_size
+        )
+        x2 = self.l2_conv_input(input_sp_tensor2)
+        x2_conv1 = self.l2_conv1(x2)
+        x2_conv2 = self.l2_conv2(x2_conv1)
+        x2_conv3 = self.l2_conv3(x2_conv2)
+        x2_conv4 = self.l2_conv4(x2_conv3)
+
+
+        pyramids = [[x0_conv1, x0_conv2, x0_conv3, x0_conv4], 
+                    [x1_conv1, x1_conv2, x1_conv3, x1_conv4], 
+                    [x2_conv1, x2_conv2, x2_conv3, x2_conv4]]
+
+        obo_convs = [[self.obo_0_0, self.obo_0_1, self.obo_0_2, self.obo_0_3], 
+                     [self.obo_1_0, self.obo_1_1, self.obo_1_2, self.obo_1_3], 
+                     [self.obo_2_0, self.obo_2_1, self.obo_2_2, self.obo_2_3]]
+
+        final_convs = [self.final_convs_0,  self.final_convs_1, self.final_convs_2, self.final_convs_3]
+    
+        I = len(pyramids)
+        J = len(pyramids[0])
+        for i in list(range(I))[::-1]:
+            for j in list(range(J))[::-1]: 
+                print(i, j)
+                base = obo_convs[i][j](pyramids[i][j])
+                if i != I-1:
+                    pass
+                    #base = spconv.SparseConvTensor.from_dense(nn.Upsample(size=base.dense().size()[2:], mode='nearest')(pyramids[i+1][j].dense()) +  base.dense())
+                    up = pyramids[i+1][j]
+                    for ii,_ in enumerate(up.spatial_shape):
+                        up.spatial_shape[ii] *= 2
+                    up = self.pool_2(up)
+                    up.indices =  up.indices * torch.tensor([1,  2, 2, 2], device=base.features.device, dtype=torch.int32)
+
+                    print(base.indices.size())
+                    print(torch.unique(base.indices, dim=0).size())
+                    print(up.indices.size())
+                    print(torch.unique(up.indices, dim=0).size())
+
+                    print (torch.max(up.indices, dim=0))
+                    print (torch.max(base.indices, dim=0))
+
+                    base.features =  torch.cat((base.features, up.features), dim = 0)
+                    base.indices =  torch.cat((base.indices, up.indices), dim = 0)
+                    print(base.indices.size())
+                    print(torch.unique(base.indices, dim=0).size())
+                if j != J-1:
+                    pass
+                    #base  += nn.Upsample(size=base.size()[2:], mode='nearest')(pyramids[i][j+1])
+                if i == 0:
+                    base = final_convs[j](base)
+                pyramids[i][j] = base
+
+        x_conv1, x_conv2, x_conv3, x_conv4  = pyramids[0]
         out = self.conv_out(x_conv4)
 
         batch_dict.update({
